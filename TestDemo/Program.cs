e113b8fa823994a693f5bd4fa6554ff1b1cd3b08
Mihály Urbanics <umisu80@gmail.com>
2021-05-09 12:49:43 +0200

    first version

---
+++ TestDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathFormula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
